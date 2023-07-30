6649164cd5c4124dd753cfd1af47a6d02b0b762f
ognjenmarkovicc <ognjen.markovicc@gmail.com>
2023-07-29 22:13:35 -0400

    Spatial emission distribution script

---
+++ scripts/spatial_emission.jl
@@ -0,0 +1,71 @@
+"""
+Example script to 
+replicate Figure 3 (a) top left of, PRR 125, 263601 (2020),
+which represents the spatial distribution of emitted 
+photons from an atomic array for different times and emission angles.
+""" 
+
+import CollectiveSpins as cs
+import CollectiveAtomArray as caa
+
+using Plots
+using ModelingToolkit, OrdinaryDiffEq
+using LinearAlgebra
+using Printf
+
+# parameters
+N = 10 # number of atoms
+lattice_spacing = 0.1
+dipole = normalize([0., 1, 1im])
+time_start = 0.0
+time_end = 20.0 
+
+system = cs.SpinCollection(cs.geometry.chain(lattice_spacing, N), 
+                           dipole, 1.); # Γ₀=1.
+
+odesys, u0, param_subs = caa.get_system_ode(system)
+
+prob = ODEProblem(odesys,u0,(time_start,time_end),param_subs)
+
+# Solve
+sol = solve(prob,RK4())
+
+# Plot spatial distribution
+mean_correlation = caa.get_mean_correlation(system, sol);
+
+# pos: shape (3, N) matrix of spin positions
+pos =  caa.get_system_pos(system)
+
+ϕ = 0
+
+thetas = collect(-π/2:0.1:π/2);
+mean_intensity = zeros(ComplexF64, length(thetas), length(sol.t));
+
+for (idx, θ) in enumerate(thetas)
+
+    # unit (θ, ϕ) direction vector r̂
+    unit_r = caa.direction_vec(θ, ϕ)
+
+    # all exp(-kr̂⋅(rᵢ-rⱼ)) factors in a (N, N) matrix
+    compl_factors = caa.get_compl_factors(θ, ϕ, pos)
+
+    # the dipole emission factor
+    dip_factor = caa.get_emission_factor(θ, ϕ, dipole)
+
+    mean_intensity[idx, :] = dip_factor*sum(compl_factors .* mean_correlation)
+
+end
+
+# plot only the selected times
+selected_times = collect(0:2:20)
+time_indices = [argmin(abs.(sol.t .- t)) for t in selected_times]
+
+# normalize by the mean intensity at time 0
+norm_intensity = real(mean_intensity)[:, time_indices]/real(mean_intensity[1, 1])
+
+labels = [@sprintf "%.1f Γt" t 
+          for t in sol.t[time_indices]]
+
+g = plot(thetas, norm_intensity, 
+          xlabel="θ", ylabel="Mean normalized intensity", legend=true,
+          yscale=:log10, label=reshape(labels, (1, :)))        
